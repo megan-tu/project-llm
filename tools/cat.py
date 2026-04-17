32f115998fcb2cd0d31beebeb693d27da1e9327e
Megan Tu <megantu@Megans-MacBook-Air.local>
2026-04-17 05:53:52 -0700

    test

--- tools/cat.py
+++ tools/cat.py
@@ -4,7 +4,7 @@ from tools.util import is_path_safe
 def cat(file):
     '''
     Opens a file and outputs its contents as a string.
-    >>> cat('docsum/Trump_tweets_graph.png')
+    >>> cat('/Users/megantu/CSCI040/docsum/Trump_tweets_graph.png')
     'UnicodeDecodeError'
     >>> cat('tool.py')
     'FileNotFoundError'
