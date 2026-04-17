3363b5b4dc6463d84d1c30495b2e5835900e5c10
Megan Tu <megantu@Megans-MacBook-Air.local>
2026-04-17 04:30:19 -0700

    test

--- tools/cat.py
+++ tools/cat.py
@@ -4,7 +4,7 @@ from tools.util import is_path_safe
 def cat(file):
     '''
     Opens a file and outputs its contents as a string.
-    >>> cat('.coverage')
+    >>> cat('CSCI040/lab-password-cracking/whitehouse_secrets.zip')
     'UnicodeDecodeError'
     >>> cat('tool.py')
     'FileNotFoundError'
