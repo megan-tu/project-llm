4191771a65b14d8c583d8c17ce399ae9cd400b61
Megan Tu <megantu@Megans-MacBook-Air.local>
2026-04-17 06:11:36 -0700

    update

--- chat.py
+++ chat.py
@@ -39,7 +39,7 @@ class Chat:
     >>> cat('..')
     'Error: unsafe path'
     >>> ls('')
-    'README.md __pycache__ chat.py encoded.txt pyproject.toml requirements.txt shanghai_communique.chinese.utf8 test_projects tools'
+    'README.md __pycache__ chat.py pyproject.toml requirements.txt test_projects tools'
     >>> ls('tools')
     'tools/__pycache__ tools/calculate.py tools/cat.py tools/grep.py tools/ls.py tools/util.py'
     >>> ls('..')
