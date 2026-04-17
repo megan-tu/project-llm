930d233430121630fea79d655c487cbb7d7f982e
Megan Tu <megantu@Megans-MacBook-Air.local>
2026-04-17 04:23:26 -0700

    doctest

--- tools/ls.py
+++ tools/ls.py
@@ -7,11 +7,7 @@ def ls(folder=None):
     Lists files in the current directory.
 
     >>> ls('')
-    'README.md __pycache__ build chat.py
-    cmc_csci040_MeganTu.egg-info dist htmlcov markdown-project
-    megan-tu.github.io pyproject.toml requirements.txt
-    test_projects tools venv'
-
+    'README.md __pycache__ chat.py pyproject.toml requirements.txt test_projects tools'
     >>> ls('tools')
     'tools/__pycache__ tools/calculate.py
     tools/cat.py tools/grep.py tools/ls.py tools/util.py'
