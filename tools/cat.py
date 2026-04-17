6da0581641c2af6604f322bb901c4d8bac5aa341
Megan Tu <megantu@Megans-MacBook-Air.local>
2026-04-17 05:48:31 -0700

    new

--- tools/cat.py
+++ tools/cat.py
@@ -4,7 +4,7 @@ from tools.util import is_path_safe
 def cat(file):
     '''
     Opens a file and outputs its contents as a string.
-    >>> cat('encoded.txt')
+    >>> cat('Trump_tweets_graph.png')
     'UnicodeDecodeError'
     >>> cat('tool.py')
     'FileNotFoundError'
