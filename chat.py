8367141878441bc22dc2d2644c0235a89e279226
Megan Tu <megantu@Megans-MacBook-Air.local>
2026-04-17 03:58:55 -0700

    update

--- chat.py
+++ chat.py
@@ -62,6 +62,7 @@ class Chat:
         Initializes the chat with default system prompt
         and tool definitions.
         '''
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
         self.MODEL = 'openai/gpt-oss-120b'
         self.messages = [
             {
