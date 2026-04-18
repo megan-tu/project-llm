90c62a9cfa30fb8c40c0839a4ef9703ec32ddb81
Megan Tu <megantu@Megans-MacBook-Air.local>
2026-04-18 10:52:32 -0700

    coverage

--- tools/cat.py
+++ tools/cat.py
@@ -13,6 +13,8 @@ def cat(file):
     'FileNotFoundError'
     >>> cat('..')
     'Error: unsafe path'
+    >>> cat('tools/util.py')
+    'import os\\n\\n\\ndef is_path_safe(path):\\n    \\'\\'\\'\\n    Returns True if the path is safe (no absolute paths or traversal).\\n    >>> is_path_safe(\\'tools/ls.py\\')\\n    True\\n    >>> is_path_safe(\\'/etc/passwd\\')\\n    False\\n    >>> is_path_safe(\\'../secrets.py\\')\\n    False\\n    >>> is_path_safe(\\'src/../config.json\\')\\n    False\\n    \\'\\'\\'\\n    if os.path.isabs(path):\\n        return False\\n\\n    if ".." in path:\\n        return False\\n\\n    else:\\n        return True\\n'
     '''
 
     if not is_path_safe(file):
